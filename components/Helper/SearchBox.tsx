d000806a1977447201b0c9706928d2a0de54d4b3
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-11-23 20:12:06 +0100

    thanks

--- components/Helper/SearchBox.tsx
+++ components/Helper/SearchBox.tsx
@@ -5,9 +5,9 @@ import { HiAdjustmentsHorizontal } from 'react-icons/hi2'
 const SearchBox = () => {
   return (
     <div className='w-full md:w-[80%] mx-auto bg-white h-[4rem] sm:h-[5rem] flex px-4 sm:px-8 flex-col justify-center rounded-lg'>
-        <div className="flex items-center justify-between h-full">
-            <input type="text" placeholder='Enter an address, city or ZIP to buy' className="sm:flex-[0.8] h-[60%] bg-gray-100 block rounded-lg outline-none px-4 placeholder:text:text-sm"/>
-            <div className="flex items-center flex-[0.2] ml-8 space-x-6">
+        <div className="flex items-center h-full">
+            <input type="text" placeholder='Enter an address, city or ZIP to buy' className="sm:flex-[0.8] h-[60%] bg-gray-100 block rounded-lg outline-none px-4 placeholder:text-sm sm:w-auto"/>
+            <div className="flex items-center flex-[0.2] ml-8 space-x-12">
                 <div className="lg:flex hidden items-center cursor-pointer space-x-2">
                     <HiAdjustmentsHorizontal className='text-gray-700 w-6 h-6'/>
                     <p className="text-gray-700 font-semibold">Advanced</p>
